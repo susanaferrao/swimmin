9dc5809e1333c4a537c56a51ba3df35803e27891
susanaferrao <susanaferrao@gmail.com>
2026-03-11 09:21:19 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE = 'swimmin-v1';
+const CACHE = 'swimmin-v3';
 const FILES = ['./swimmin-fpn.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
